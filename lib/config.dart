0d43849e575c6bc11f3286d723c9e062cddc10b0
muthasirMohamet <muthasirmohamed0@gmail.com>
2025-01-30 09:55:27 +0300

    wax kabadalka filasha ku jira lib folder-ka

--- lib/config.dart
+++ lib/config.dart
@@ -1,1 +1,1 @@
-const String baseUrl = 'http://192.168.100.37:3000'; // Replace with the correct IP address and port
+const String baseUrl = 'http://192.168.213.216:3000'; // Replace with the correct IP address and port
